4b51b40c4c45e42c0e05764cf6485ea2ab81779a
Виталий Кушла <KushlaVR@gmail.com>
2020-04-15 21:28:06 +0300

    add test project

---
+++ WemosRemote/Test/Test.ino
@@ -0,0 +1,39 @@
+﻿/*
+ Name:		Test.ino
+ Created:	4/15/2020 9:03:21 PM
+ Author:	Віталік
+*/
+
+#include "PCF8574.h"
+
+
+#define pinI2C_SCL D1 //pcf8574
+#define pinI2C_SDA D2 //pcf8574
+
+PCF8574 portExt = PCF8574(0x27);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(115200);
+	Serial.println("");
+	Serial.println("");
+	Serial.println("Start...");
+
+	portExt.begin(pinI2C_SDA, pinI2C_SCL);
+	portExt.write8(0x00);
+
+
+	pinMode(BUILTIN_LED, OUTPUT);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	delay(1000);
+	portExt.write8(0x00);
+	digitalWrite(BUILTIN_LED, HIGH);
+	delay(1000);
+	portExt.write8(0xFF);
+	digitalWrite(BUILTIN_LED, LOW);
+	Serial.println("step...");
+
+}
--- tools/copy.bat
+++ tools/copy.bat
@@ -1,3 +1,4 @@
 COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\WemosRemote\esp8266_d1_mini\Release\WemosRemote.ino.bin" firmware.bin
 COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\WemosRemote\esp8266_d1_mini\Release\8ef9dc2d6ccc632bd183ad5e6a315ab5\WemosRemote.ino.spiffs.bin" setup.bin
+COPY "c:\users\%username%\AppData\Local\Temp\VMBuilds\Test\esp8266_d1_mini\Release\Test.ino.bin" test.bin
 pause
