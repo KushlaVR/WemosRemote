b359158e2284ccb41b721281a6c22890a3645f64
Виталий Кушла <KushlaVR@gmail.com>
2020-06-04 21:51:30 +0300

    marge

--- WemosRemote/WemosRemote/__vm/.WemosRemote.vsarduino.h
+++ WemosRemote/WemosRemote/__vm/.WemosRemote.vsarduino.h
@@ -1,9 +1,10 @@
 /* 
 	Editor: https://www.visualmicro.com/
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: LOLIN(WEMOS) D1 R2 & mini, Platform=esp8266, Package=esp8266
 */
@@ -12,17 +13,14 @@
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __ESP8266_ESp8266__
+#define __ESP8266_esp8266__
 #define __ESP8266_ESP8266__
-#define _VMDEBUG 1
 #define __ets__
 #define ICACHE_FLASH
-#define F_CPU 160000000L
+#define NONOSDK22x_191024 1
+#define F_CPU {build.f_cpu}
 #define LWIP_OPEN_SRC
-#define TCP_MSS 536
-#define LWIP_FEATURES 1
-#define LWIP_IPV6 0
-#define ARDUINO 108012
+#define ARDUINO 108010
 #define ARDUINO_ESP8266_WEMOS_D1MINI
 #define ARDUINO_ARCH_ESP8266
 #define FLASHMODE_DIO
@@ -97,7 +95,7 @@ typedef long __PTRDIFF_TYPE__;
 #include "Esp.h"
 
 
-#include <arduino.h>
+#include "arduino.h"
 #include <pins_arduino.h> 
 
 #include "..\generic\Common.h"
